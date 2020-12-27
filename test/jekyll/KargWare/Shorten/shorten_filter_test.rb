8d699e66deffbbb9cb0de6c018f379b90a5b3308
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2020-12-27 11:50:15 +0100

    Add Jekyll Filter test

---
+++ test/jekyll/KargWare/Shorten/shorten_filter_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+# require 'jekyll/KargWare/shorten'
+
+module Jekyll
+  module KargWare
+    module Shorten
+      # class ShortenFilterTest < Minitest::Test
+      #   include Jekyll::KargWare::Shorten::ShortenFilter
+
+      # end
+    end
+  end
+end
